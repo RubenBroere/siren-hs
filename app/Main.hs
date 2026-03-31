a8d4e3235d820b88c2cdd7c15470fb9cbd3c49bf
Olaf Boekholt <olafboekholt@gmail.com>
2026-03-31 22:56:45 +0200

    add samples for presentation

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,45 @@ module Main (main) where
 
 import Control.Monad (when)
 import Data.Either (isLeft)
+import Options.Applicative
 import System.Directory (createDirectoryIfMissing)
 
-import Samples (writeSampleSvg)
+import Samples (buggedGraph, sampleGraph)
+import Siren (Direction (TopDown), Graph, writeGraphSvg)
+
+graphSelectionParser :: Parser String
+graphSelectionParser = strArgument (metavar "GRAPH_NAME")
+
+graphSelectionInfo :: ParserInfo String
+graphSelectionInfo =
+    info
+        (graphSelectionParser <**> helper)
+        ( fullDesc
+            <> progDesc "Render a named graph as SVG"
+            <> header "siren-hs graph renderer"
+        )
+
+graphForSelection :: String -> Either String (String, FilePath, Either String Graph)
+graphForSelection selection =
+    case selection of
+        "sampleGraph" -> Right ("sampleGraph", "output/sample.svg", sampleGraph)
+        "buggedGraph" -> Right ("buggedGraph", "output/bugged.svg", buggedGraph)
+        _ -> Left "Expected one of: sampleGraph, buggedGraph"
 
 main :: IO ()
 main = do
+    selection <- execParser graphSelectionInfo
     createDirectoryIfMissing True "output"
-    result <- writeSampleSvg "output/sample.svg"
+    result <-
+        case graphForSelection selection of
+            Left err -> pure (Left err)
+            Right (selectionName, outputPath, selectedGraph) ->
+                case selectedGraph of
+                    Left graphErr -> pure (Left graphErr)
+                    Right graph -> do
+                        writeGraphSvg outputPath TopDown graph
+                        pure (Right (selectionName, outputPath))
     case result of
-        Left err -> putStrLn ("Failed to render sample: " <> err)
-        Right path -> putStrLn ("Rendered sample SVG: " <> path)
-    when (isLeft result) (fail "siren-hs sample render failed")
+        Left err -> putStrLn ("Failed to render graph: " <> err)
+        Right (selectionName, path) -> putStrLn ("Rendered " <> selectionName <> " SVG: " <> path)
+    when (isLeft result) (fail "siren-hs render failed")
--- app/Samples.hs
+++ app/Samples.hs
@@ -1,6 +1,6 @@
 module Samples
     ( sampleGraph
-    , writeSampleSvg
+    , buggedGraph
     )
 where
 
@@ -9,22 +9,28 @@ import Siren
 sampleGraph :: Either String Graph
 sampleGraph =
   buildGraph
-    ( roundedNode "start" "Start"
-        <> node "read" "Read Input"
-        <> diamondNode "valid" "Valid?"
-        <> node "process" "Process"
-        <> roundedNode "done" "Done"
-        <> edge "start" "read"
-        <> edge "read" "valid"
-        <> edgeWithLabel "valid" "process" (Just "yes")
-        <> edgeWithLabel "valid" "done" (Just "no")
-        <> edge "process" "done"
+    (  diamondNode "A" "Start"
+    <> node "B" "Left"
+    <> node "C" "Right"
+    <> roundedNode "D" "Down"
+    <> roundedNode "E" "End"
+    <> edgeWithLabel "A" "B" (Just "go left")
+    <> edgeWithLabel "A" "C" (Just "go right")
+    <> edge "B" "D"
+    <> edge "B" "E"
     )
 
-writeSampleSvg :: FilePath -> IO (Either String FilePath)
-writeSampleSvg outputPath =
-    case sampleGraph of
-        Left err -> pure (Left err)
-        Right graph -> do
-            writeGraphSvg outputPath TopDown graph
-            pure (Right outputPath)
+buggedGraph :: Either String Graph
+buggedGraph =
+  buildGraph
+    (  roundedNode "start" "Start"
+    <> node "read" "Read Input"
+    <> diamondNode "valid" "Valid?"
+    <> node "process" "Process"
+    <> roundedNode "done" "Done"
+    <> edge "start" "read"
+    <> edge "read" "valid"
+    <> edgeWithLabel "valid" "process" (Just "yes")
+    <> edgeWithLabel "valid" "done" (Just "no")
+    <> edge "process" "done"
+    )
